4b1e46a3441b319b79bcfd461b69d9c48f82a3e8
Jefferson <acrifagia@yahoo.com.br>
2019-06-02 12:16:54 -0300

    Toggle Students list

--- js/script.js
+++ js/script.js
@@ -25,12 +25,46 @@ function hideLabel(status){
     document.getElementById('pickAnOptionLabel').style.display= status;
 }
 
+function toggleStudents(element){
+    //remove from list onclick
+    element.onclick = function() {
+        var parent = element.parentNode.id;
 
-function moveOnClick(element){
-    var allStudents = document.getElementById('allStudents');
-    element.remove();
-
+        if (parent == 'allStudents'){
+            var classes = document.getElementById('classes');
+            this.parentNode.removeChild(this);
+            classes.appendChild(this);
+            parent = '';
+        }
+        else if(parent == 'classes'){
+            var allStudents = document.getElementById('allStudents');
+    
+            element.onclick = function() {
+                this.parentNode.removeChild(this);
+                allStudents.appendChild(this);
+                parent = '';
+            }
+        }
+    }
 }
+
+// function removeFromClass(element){
+//     var allStudents = document.getElementById('allStudents');
+//     element.onclick = function() {
+//         this.parentNode.removeChild(this);
+//         allStudents.appendChild(this);
+//     }
+//     // var classes = document.getElementById('classes');
+//     // var listItems = classes.getElementsByTagName("a"); 
+//     // var allStudents = document.getElementById('allStudents');
+//     // for (var i = 0; i < listItems.length; i++) {
+//     //   listItems[i].onclick = function() {
+//     //       this.parentNode.removeChild(this);
+//     //       allStudents.appendChild(this);
+//     //     }
+//     // }
+
+// }
 // var imgBtn = document.getElementsByClassName('profilePicture');
 // var fileInp = document.querySelector('[type="file"]');
 
